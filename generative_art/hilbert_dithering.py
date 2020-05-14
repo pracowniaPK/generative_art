19af3c16ffaf2b032676b5e3d2c6e9fcd2e34a31
pracowniaPK <kaczory5@gmail.com>
2020-05-14 20:11:27 +0200

    add custom thresholds in dithering

--- generative_art/hilbert_dithering.py
+++ generative_art/hilbert_dithering.py
@@ -4,17 +4,18 @@ from PIL import Image, ImageDraw
 from math import log2, ceil
 
 
-def hilbert_dithering(im_in, verbose=True):
+def hilbert_dithering(im_in, thresholds=[0, 255], verbose=True):
     im_out = im_in.copy()
     im_width, im_height = im_out.size
+    p = ceil(log2(max(im_width, im_height)))
     if im_out.mode in ('RGB', 'RGBA'):
         ch = 3
     if im_out.mode in ('L', 'LA'):
         ch = 1
     if verbose:
         print('picture size:', im_width, im_height)
+        print('p-size:', p)
         print('channels:', ch)
-    p = ceil(log2(max(im_width, im_height)))
     n = 2
     h = HilbertCurve(p, n) 
     err = [0] * ch
@@ -25,10 +26,17 @@ def hilbert_dithering(im_in, verbose=True):
             new_pixel = [0] * ch + [255]
             for j in range(ch):
                 current_v = im_out.getpixel(tuple(coords))[j]
-                if current_v + err[j] < + 255/2:
-                    new_v = 0
-                else:
-                    new_v = 255
+                prev_t = thresholds[0]
+                prev_score = abs(current_v + err[j] - prev_t)
+                new_v = 0
+                for t in thresholds:
+                    score = abs(current_v + err[j] - t)
+                    if score > prev_score:
+                        new_v = prev_t
+                        break
+                    prev_score = score
+                    prev_t = t
+                    new_v = t
                 new_pixel[j] = new_v
                 err[j] += current_v - new_v
             new_pixel = tuple(new_pixel)
@@ -38,9 +46,10 @@ def hilbert_dithering(im_in, verbose=True):
 
 
 if __name__ == "__main__":    
-    filename = 'p2.png'
-    im_in = Image.open('img_in/' + filename).convert('LA')
-    im_out = hilbert_dithering(im_in)
-    im_out.save('img_out/hilbert/' + filename)
+    filename = 'l1'
+    ext = 'jpg'
+    im_in = Image.open('img_in/' + filename + '.' + ext).convert('LA')
+    im_out = hilbert_dithering(im_in, thresholds=[0, 16, 32, 64, 128, 255])
+    im_out.save('img_out/hilbert/' + filename + '.png')
     # im_out.show()
 
